66871de34ddcda5936f5efca4340a3f437b0e608
AghoraGuru <kalyangutu18@gmail.com>
2022-02-14 13:07:19 +0530

    just commented

--- Eoc&Oop/Java Files/HardwareSimulator.java
+++ Eoc&Oop/Java Files/HardwareSimulator.java
@@ -1,17 +1,14 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class HardwareSimulator {
 
 
     public static void main(String[] args) {
         
-        int[] a = {0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+       /* int[] a = {0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         int[] b = {1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         int[] c = {0, 0, 1, 0, 0, 0, 0, 0};
-        int[] in = {1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1};
+        int[] in = {1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1};*/
         //System.out.println(Arrays.toString(Adders.Add16(a, b)));
         //int[] x = ProgramCounter.pc(in, 0, 1, 0);
         //System.out.println(Arrays.toString(ProgramCounter.pc(ProgramCounter.pc(x, 1, 0, 0), 1, 0, 0)));
